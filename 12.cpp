e67a9d1b95c86be4ce3fd9eaf9ebf17d02310f2f
aby <117300665+Abycahyo@users.noreply.github.com>
2023-01-31 03:56:15 +0700

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,16 @@
+#include<iostream> 
+using namespace std; 
+  
+int main () 
+{ 
+    // Array of strings 
+    string arr[5] = {"Windha Aby Cahyo", "zaki", 
+                     "iki", "subahul", 
+                     "eko"}; 
+  
+    cout << "Isi Array : \n"; 
+    for (int i = 0; i < 5; i++) 
+        cout << arr[i] << endl; 
+  
+    return 0; 
+}
